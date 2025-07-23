6bfdca37a2e56e7560e04efaefddfdb980ce4cb6
Emmanuel Adoum <adoumouangnamouemmanuel@gmail.com>
2025-07-23 13:55:39 +0000

    firebase config

--- mobile/firebase/config/firebaseConfig.ts
+++ mobile/firebase/config/firebaseConfig.ts
@@ -1,25 +1,14 @@
-import { initializeApp, FirebaseApp } from "firebase/app";
-import { getAuth, Auth } from "firebase/auth";
+import {
+  initializeApp,
+  getApps,
+  getApp,
+} from "firebase/app";
+import {
+  getReactNativePersistence,
+  initializeAuth,
+} from "firebase/auth";
 import { getFirestore, Firestore } from "firebase/firestore";
-
-
-
-// Note: Adjust how you access env variables depending on your setup,
-// for example process.env or imported envs for React Native.
-
-// If you use react-native-dotenv or babel-plugin-dotenv-import, import vars like:
-// import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, ... } from '@env';
-console.log("Hhhhhhhhhhhhhh", process.env.FIREBASE_API_KEY);
-
-// const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API_KEY!,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
-//   projectId: process.env.FIREBASE_PROJECT_ID!,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET!,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
-//   appId: process.env.FIREBASE_APP_ID!,
-//   measurementId: process.env.FIREBASE_MEASUREMENT_ID!,
-// };
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA0jhA21wcBD4IP6CDBDCuX7enrKi-w00M",
@@ -31,7 +20,20 @@ const firebaseConfig = {
   measurementId: "G-VKWHG505CF",
 };
 
-const app: FirebaseApp = initializeApp(firebaseConfig);
+// Initialize Firebase only if it hasn't been initialized already
+ let app;
+ if (!getApps().length) {
+   app = initializeApp(firebaseConfig);
+ } else {
+   app = getApp(); // retrieve the default app
+}
+ 
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
 
-export const auth: Auth = getAuth(app);
 export const db: Firestore = getFirestore(app);
+
+// , {
+//   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+// }
