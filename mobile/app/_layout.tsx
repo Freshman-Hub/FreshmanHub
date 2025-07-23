5ae55b88504decf138e9ac5d92df7b8771676be6
Emmanuel Adoum <adoumouangnamouemmanuel@gmail.com>
2025-07-23 02:13:16 +0000

    main layout

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,43 +1,45 @@
+import { ThemeProvider } from "@/contexts/ThemeContext";
+import { UserProvider } from "@/contexts/UserContext";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
-import { ThemeProvider } from "@/contexts/ThemeContext";
-import { UserProvider, useUser } from "@/contexts/UserContext";
 
 function LayoutContent() {
-  const { role } = useUser();
+  // const { role } = useUser();
 
   // Fallback if role not yet selected
-  const getLayoutName = () => {
-    if (!role) return "role-select"; // A screen where user picks a role
-    switch (role) {
-      case "freshman":
-        return "(freshman-tabs)";
-      case "student":
-        return "(student-tabs)";
-      case "peerCoach":
-      case "peerAdvisor":
-      case "buddy":
-        return "(coach-tabs)";
-      case "headCoach":
-        return "(head-coach)";
-      case "academicAdvisor":
-        return "(academic-advisor)";
-      case "odip":
-      case "sle":
-        return "(admin-tabs)";
-      default:
-        return "(student-tabs)";
-    }
-  };
+  // const getLayoutName = () => {
+  //   if (!role) return "role-select"; // A screen where user picks a role
+  //   switch (role) {
+  //     case "freshman":
+  //       return "(freshman-tabs)";
+  //     case "buddy":
+  //       return "(coach-tabs)";
+  //     case "odip":
+  //     case "sle":
+  //       return "(admin-tabs)";
+  //     default:
+  //       return "(student-tabs)";
+  //   }
+  // };
 
-  const layoutName = getLayoutName();
+  // const layoutName = getLayoutName();
 
   return (
     <>
-      <Stack>
-        <Stack.Screen name={layoutName} options={{ headerShown: false }} />
+      <Stack
+        initialRouteName="(splash)"
+        screenOptions={{
+          headerShown: false,
+          animation: "fade_from_bottom",
+          contentStyle: { backgroundColor: "transparent" },
+        }}
+      >
+        <Stack.Screen name="(splash)" options={{ headerShown: false }} />
+        <Stack.Screen name="(onboarding)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        {/* <Stack.Screen name={layoutName} options={{ headerShown: false }} /> */}
         <Stack.Screen name="(routes)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
