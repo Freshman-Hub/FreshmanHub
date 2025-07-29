f2e5f2cc591c5280784f2fb48fd87b865d9c714e
Emmanuel Adoum <adoumouangnamouemmanuel@gmail.com>
2025-07-29 22:05:14 +0000

    search members layout

---
+++ mobile/app/(routes)/chats/[id]/search-members/_layout.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { Stack } from "expo-router";
+import { useTheme } from "@/contexts/ThemeContext";
+
+export default function SearchMembersLayout() {
+  const { theme } = useTheme();
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        animation: "slide_from_right",
+        contentStyle: { backgroundColor: theme.colors.background },
+      }}
+    >
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
