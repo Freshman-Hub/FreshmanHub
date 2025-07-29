41f086900c6f63b2e373a3e06c6204b076e25194
Emmanuel Adoum <adoumouangnamouemmanuel@gmail.com>
2025-07-29 22:02:56 +0000

    chats layout

---
+++ mobile/app/(routes)/chats/_layout.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { Stack } from "expo-router";
+import { useTheme } from "@/contexts/ThemeContext";
+
+export default function ChatsLayout() {
+  const { theme } = useTheme();
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        animation: "slide_from_right",
+        contentStyle: { backgroundColor: theme.colors.background },
+      }}
+    >
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="select-contact/index"
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen name="[id]/index" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
