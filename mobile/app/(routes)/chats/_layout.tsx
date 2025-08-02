1c43486e8267689fbc5ab822bddda5465fb9a653
Emmanuel Adoum <adoumouangnamouemmanuel@gmail.com>
2025-08-02 17:45:28 +0000

    chats layout with chat context

--- mobile/app/(routes)/chats/_layout.tsx
+++ mobile/app/(routes)/chats/_layout.tsx
@@ -1,24 +1,25 @@
 "use client";
 import { Stack } from "expo-router";
 import { useTheme } from "@/contexts/ThemeContext";
+import { Chat } from "stream-chat-expo";
+import { chatClient } from "@/config/stream.config";
 
 export default function ChatsLayout() {
   const { theme } = useTheme();
 
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        animation: "slide_from_right",
-        contentStyle: { backgroundColor: theme.colors.background },
-      }}
-    >
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen
-        name="select-contact"
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen name="[id]/index" options={{ headerShown: false }} />
-    </Stack>
+    <Chat client={chatClient}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          animation: "slide_from_right",
+          contentStyle: { backgroundColor: theme.colors.background },
+        }}
+      >
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="select-contact" options={{ headerShown: false }} />
+        <Stack.Screen name="[id]/index" options={{ headerShown: false }} />
+      </Stack>
+    </Chat>
   );
 }
